c553120cf42ebc5e5816008ac98b8151445519d5
rickstart <ricardo.celj@gmail.com>
2014-11-24 21:30:49 -0600

    Section Number

--- app/src/main/java/com/rickstart/moviefriend/MainActivity.java
+++ app/src/main/java/com/rickstart/moviefriend/MainActivity.java
@@ -1,21 +1,17 @@
 package com.rickstart.moviefriend;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 
@@ -59,7 +55,7 @@ public class MainActivity extends ActionBarActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = getString(R.string.mobile);
                 break;
             case 2:
                 mTitle = getString(R.string.title_section2);
@@ -116,6 +112,7 @@ public class MainActivity extends ActionBarActivity
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
+        private static int section;
         /**
          * Returns a new instance of this fragment for the given section
          * number.
@@ -124,6 +121,7 @@ public class MainActivity extends ActionBarActivity
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            section = sectionNumber;
             fragment.setArguments(args);
             return fragment;
         }
@@ -135,6 +133,9 @@ public class MainActivity extends ActionBarActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            TextView section_label = (TextView) rootView.findViewById(R.id.section_label);
+            section_label.setText(ARG_SECTION_NUMBER+" "+section);
             return rootView;
         }
 
